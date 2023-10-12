35f442ef9c2d06d3b051fb57a17c8629e7cd081f
Aaron Parsons <aarpar204@gmail.com>
2023-10-12 09:11:17 -0500

    CSS Tweaks Oct 12 2023

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Home,SignUp, Login, Homepage, NewPost, EditPost, SinglePost, AboutUs } 
 import {Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import './App.css';
 
 const App = () => {
 
